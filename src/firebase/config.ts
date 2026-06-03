d1c01ed8a99460835aaf4317e6c75eb621494106
bhargav2301 <bhargavdvs2001@gmail.com>
2026-06-03 09:45:04 +0530

    fix: align firebase runtime configuration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-4575541004-97a13",
-  "appId": "1:49906139708:web:756883e71e00dd20207939",
-  "apiKey": "AIzaSyAX6vngKTQrDWgMSfJXG-ToamwC1X2vi6U",
-  "authDomain": "studio-4575541004-97a13.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "49906139708"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
